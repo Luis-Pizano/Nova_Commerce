369c7582132e9d50241dd7f8cd6b4c416f924b96
Luis Pizano <luispizano883@gmail.com>
2025-12-19 19:57:10 -0300

    Se agrega API de registro

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,55 @@ const sql = require("mssql");
 const cors = require("cors");
 const multer = require("multer");
 const bcrypt = require("bcrypt");
-const multer = require("multer");
 
 const app = express();
 
 const PORT = 8080;
 
 const storage = multer.memoryStorage();
-const upload = multer({storage});
+const upload = multer({ storage });
 
 app.use(cors());
 app.use(express.json())
 
-const multer = multer();
+const dbConfig = {
+    user: 'ADMIN',
+    password: '1234',
+    server: 'localhost',
+    database: 'NOVA_COMMERCE',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+};
+
+app.post('/api/registro', async (req, res) => {
+    const { usuario, email, password } = req.body;
+    try {
+        
+        await sql.connect(dbConfig);
+
+        const request = new sql.Request();
+
+       const passwordEncrypted =await bcrypt.hash(password,10)
+
+        request.input('usuario',sql.VarChar(),usuario);
+        request.input('email',sql.VarChar(),email);
+        request.input('password',sql.VarChar(),passwordEncrypted);
+
+        await request.query('INSERT INTO CUENTAS (USUARIO, EMAIL, PASSWORD) VALUES (@usuario, @email, @password)');
+
+        res.status(200).json({ Message: 'Resgistro exitoso.' });
+
+    } catch (error) {
+        console.error(`Error en el registro, Error: ${error}`)
+    }
 
 
-app.listen(PORT, () =>{
+
+})
+
+
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 })
---
+++ nova_commerce/src/components/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <h1 style={{ display: "flex", justifyContent: "center" }}>
+      Home
+    </h1>
+  );
+};
+
+export default Home;
