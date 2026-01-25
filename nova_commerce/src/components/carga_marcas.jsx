71e6a1c1f7a7f39d3f0fbba8bc9ab0761a8d4f95
Luis Pizano <luispizano883@gmail.com>
2026-01-24 22:53:38 -0300

    Se agrega funcionalidad de eliminar junto con  su respectivo modal de confiracion

--- nova_commerce/src/components/carga_marcas.jsx
+++ nova_commerce/src/components/carga_marcas.jsx
@@ -1,6 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
+import { useParams } from "react-router-dom";
+
 
 const Marcas = () => {
+    const { id } = useParams();
+
+    const [marcaId, setMarcaId] = useState(null);
+
     const [formdata, setFormdata] = useState({
         nombre: ""
     });
@@ -8,6 +14,8 @@ const Marcas = () => {
     const [marcas, setMarcas] = useState([]);
 
     const [modalOpen, setModalOpen] = useState(false);
+
+    const [modalOpenDelete, setModalOpenDelete] = useState(false);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -20,7 +28,7 @@ const Marcas = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:8080/api/cargar_marcas',{
+            const response = await fetch('http://localhost:8080/api/cargar_marcas', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formdata)
@@ -36,9 +44,7 @@ const Marcas = () => {
             console.error(`Error al cargar marca, Error: ${error}`);
         }
     };
-
-    useEffect(() => {
-        const fetchMarcas = async () => {
+    const fetchMarcas = async () => {
         try {
             const response = await fetch('http://localhost:8080/api/obtener_marcas');
             const data = await response.json();
@@ -49,10 +55,72 @@ const Marcas = () => {
             console.error(`Error al obtener marcas, Error: ${error}`);
         }
     };
-    fetchMarcas();
+
+    useEffect(() => {
+        fetchMarcas();
     }, []);
 
-    const Modal = () => setModalOpen(!modalOpen);
+    const Modal = (marca) => {
+        setModalOpen(true);
+        setMarcaId(marca.ID);
+        setFormdata({
+            nombre: marca.NOMBRE
+        });
+    }
+
+    const CloseModal = () => {
+        setModalOpen(false);
+        setFormdata({
+            nombre: ""
+        });
+    }
+
+    const CloseDeleteModal = () => {
+        setModalOpenDelete(false);
+        setFormdata({
+            nombre: ""
+        });
+    }
+
+    const openDeleteModal = (marca) => {
+        setMarcaId(marca.ID);
+        setFormdata({ nombre: marca.NOMBRE });
+        setModalOpenDelete(true);
+    };
+    const Actualizar_Marca = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:8080/api/actualizar_marca/${marcaId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formdata)
+            })
+            if (response.ok) {
+                console.log('Marca actualizada exitosamente.');
+                setFormdata({
+                    nombre: ""
+                });
+                fetchMarcas();
+                CloseModal();
+            }
+        } catch (error) {
+            console.error(`Error al actualizar la marca, Error: _${error}`);
+        }
+    }
+
+    const Delete_Marca = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/eliminar_marca/${marcaId}`, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                console.log('Marca eliminada exitosamente.');
+                fetchMarcas();
+            }
+        } catch (error) {
+            console.error(`Error al eliminar la marca, Error: ${error}`);
+        }
+    }
 
     return (
         // Carga de Marcas
@@ -71,17 +139,19 @@ const Marcas = () => {
                 <table className="border-collapse border border-black ">
                     <thead>
                         <tr>
+                            <th className="border border-black px-4 py-3 text-lg">ID</th>
                             <th className="border border-black px-4 py-3 text-lg">Nombre</th>
                             <th className="border border-black px-4 py-3 text-lg">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
                         {marcas.map((marca) => (
-                            <tr>
+                            <tr key={marca.ID}>
+                                <td className="border border-black px-4 py-3">{marca.ID}</td>
                                 <td className="border border-black px-4 py-3">{marca.NOMBRE}</td>
                                 <td className="border border-black px-4 py-3">
-                                    <button className="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 mr-2" onClick={Modal}>Editar</button>
-                                    <button className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600">Eliminar</button>
+                                    <button className="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 mr-2" onClick={() => Modal(marca)}>Editar</button>
+                                    <button className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600" onClick={() =>  openDeleteModal(marca)}>Eliminar</button>
                                 </td>
                             </tr>
                         ))}
@@ -93,12 +163,26 @@ const Marcas = () => {
                 <div className="fixed inset-0 flex items-center justify-center bg-black/40 bg-opacity-50">
                     <div className="bg-white p-6 rounded shadow-md">
                         <h2 className="text-xl font-bold mb-4">Editar Marca</h2>
-                        <form>
+                        <form onSubmit={Actualizar_Marca}>
                             <label htmlFor="editNombre" className="block mb-2">Nombre de la marca</label>
-                            <input type="text" id="editNombre" className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4" placeholder="Nombre de la marca" />
+                            <input type="text" id="editNombre" name="nombre" className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4" value={formdata.nombre} onChange={handleChange} placeholder="Nombre de la marca" />
                             <div className="flex justify-between">
-                                <a className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600" onClick={Modal}> Cancelar</a>
-                                <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Actualizar</button>
+                                <a className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 cursor-pointer" onClick={CloseModal}> Cancelar</a>
+                                <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 cursor-pointer">Actualizar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            {modalOpenDelete && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/40 bg-opacity-50">
+                    <div className="bg-white p-6 rounded shadow-md">
+                        <h2 className="text-xl font-bold mb-4">Eliminar Marca {formdata.nombre}</h2>
+                        <form onSubmit={Delete_Marca}>
+                            <div className="flex justify-between">
+                                <a className="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 cursor-pointer" onClick={CloseDeleteModal}> Cancelar</a>
+                                <button type="submit" className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 cursor-pointer">Eliminar</button>
                             </div>
                         </form>
                     </div>
